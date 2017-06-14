8b8be67cf791fcc47c68670dfb6358f802564b3f
cj2978 <chris@thejonesathome.co.uk>
2017-06-14 12:44:06 +0100

    Add the Price Functionality
    Add the functionality to load the price details from an external source file allowing for modification.

--- CheckoutService/CheckoutLogic/Checkout.cs
+++ CheckoutService/CheckoutLogic/Checkout.cs
@@ -1,10 +1,14 @@
 ﻿using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
 
 namespace CheckoutLogic
 {
     public static class Checkout
     {
         private static Dictionary<char, int> _items = new Dictionary<char, int>();
+        private static List<Price> _pricelist = new List<Price>();
+
 
         public static void ScanItem(string item)
         {
@@ -25,9 +29,19 @@ namespace CheckoutLogic
 
         public static int GetTotal()
         {
+            GetPriceList();
+
             var total = 0;
 
             return total;
+        }
+
+        private static void GetPriceList()
+        {
+            using (var reader = new StreamReader(@"C:\Users\Christopher\Documents\GitHub\Checkout2\CheckoutService\bin\prices.json"))
+            {
+                _pricelist = JsonConvert.DeserializeObject<List<Price>>(reader.ReadToEnd());
+            }
         }
     }
 }
